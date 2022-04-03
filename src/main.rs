8df82fca1912ed7d5b1010145176bec0fb4cc33e
teobler <teobler@163.com>
2022-04-03 17:22:33 +0800

    add the first test case.
    Signed-off-by: teobler <teobler@163.com>

--- src/main.rs
+++ src/main.rs
@@ -6,3 +6,31 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("{:#?}", resp);
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use httpmock::prelude::{MockServer, GET};
+    use serde_json::json;
+
+    #[derive(serde::Deserialize, Debug)]
+    struct TestStruct {
+        test: String,
+    }
+
+    #[test]
+    fn it_works() -> Result<(), Box<dyn std::error::Error>> {
+        let server = MockServer::start();
+        let test_mock = server.mock(|when, then| {
+            when.method(GET).path("/test");
+            then.status(200)
+                .header("content-type", "text/html")
+                .json_body(json!({ "test": "ohi" }));
+        });
+
+        let response = reqwest::blocking::get(server.url("/test"))?.json::<TestStruct>()?;
+
+        test_mock.assert();
+        assert_eq!(response.test, "ohi");
+        Ok(())
+    }
+}
